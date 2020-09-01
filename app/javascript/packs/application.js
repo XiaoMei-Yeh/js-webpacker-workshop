a012332737890fa1cd3fcd7d81c240cfaa5fcdd4
Mei <yeha90822@gmail.com>
2020-09-01 20:02:35 +0800

    task1-2-3done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,21 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery'
+import 'jquery/dist/jquery.js'
+import $ from 'jquery'
+window.$ = $
+import 'select2'
+import 'select2/dist/css/select2.css'
+import Swal from 'sweetalert2'
+window.Swal = Swal
+import 'font-awesome/css/font-awesome.css'
+import 'tempusdominus-bootstrap-4' 
+
+
+
+
+document.addEventListener('turbolinks: load', function() {
+  
+})
+
